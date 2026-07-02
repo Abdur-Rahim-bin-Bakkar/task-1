44444581c0033f432f6c263811fc47a56dd8484a
Abdur-Rahim-bin-Bakkar <webdesignrahim4061@gmail.com>
2026-07-02 20:56:30 +0600

    install hero ui

--- src/app/page.js
+++ src/app/page.js
@@ -11,20 +11,23 @@ export default function Home() {
 
   return (
     <main className="min-h-screen bg-gray-100 py-10">
-      <div className="max-w-2xl mx-auto bg-white shadow-lg rounded-xl p-8">
-        <h1 className="text-3xl font-bold text-center mb-8">
-          Item Calculator
-        </h1>
+      <div className="flex ">
 
-        <ItemForm
-          items={items}
-          setItems={setItems}
-          setResult={setResult}
-        />
+        <div className="max-w-2xl mx-auto bg-white shadow-lg rounded-xl p-8">
+          <h1 className="text-3xl font-bold text-center mb-8">
+            Item Calculator
+          </h1>
 
-        <ItemList items={items} />
+          <ItemForm
+            items={items}
+            setItems={setItems}
+            setResult={setResult}
+          />
 
-        <ResultCard result={result} />
+          <ItemList items={items} />
+
+          <ResultCard result={result} />
+        </div>
       </div>
     </main>
   );
